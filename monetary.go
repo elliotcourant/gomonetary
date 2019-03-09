627f8fb6c0312c2f54db43e17688e7929982bf44
Elliot Courant <elliot.courant@readystock.io>
2019-03-08 20:04:36 -0600

    Updated readme and added documentation on cache.

--- monetary.go
+++ monetary.go
@@ -5,14 +5,18 @@ import (
 	"strings"
 )
 
-func Parse(moneyString, locale string) (float64, error) {
+func Parse(value, locale string) (float64, error) {
 	meta, ok := metaData[strings.ToLower(locale)]
 	if !ok {
 		return 0, fmt.Errorf("locale [%s] is not valid or is not supported", locale)
 	}
-	return parseMoney(meta, moneyString)
+	return parseMoney(meta, value)
 }
 
-func ParseBytes(moneyBytes []byte, locale string) (float64, error) {
-	return Parse(string(moneyBytes), locale)
+func Format(value float64, locale string) (string, error) {
+	meta, ok := metaData[strings.ToLower(locale)]
+	if !ok {
+		return "", fmt.Errorf("locale [%s] is not valid or is not supported", locale)
+	}
+	return formatMoney(meta, value)
 }
--- monetary_test.go
+++ monetary_test.go
@@ -64,24 +64,46 @@ func TestParse(t *testing.T) {
 	}
 	for _, val := range values {
 		t.Run(fmt.Sprintf("Locale:%s", val.Locale), func(t *testing.T) {
-			t.Run("String", func(t *testing.T) {
-				result, err := Parse(val.Input, val.Locale)
-				if val.ExpectedError != nil {
-					assert.EqualError(t, err, val.ExpectedError.Error())
-				} else {
-					assert.NoError(t, err, "no error should be returned")
-				}
-				assert.Equal(t, val.Expected, result, "should match expected")
-			})
-			t.Run("Bytes", func(t *testing.T) {
-				result, err := ParseBytes([]byte(val.Input), val.Locale)
-				if val.ExpectedError != nil {
-					assert.EqualError(t, err, val.ExpectedError.Error())
-				} else {
-					assert.NoError(t, err, "no error should be returned")
-				}
-				assert.Equal(t, val.Expected, result, "should match expected")
-			})
+			result, err := Parse(val.Input, val.Locale)
+			if val.ExpectedError != nil {
+				assert.EqualError(t, err, val.ExpectedError.Error())
+			} else {
+				assert.NoError(t, err, "no error should be returned")
+			}
+			assert.Equal(t, val.Expected, result, "should match expected")
+		})
+	}
+}
+
+func TestFormat(t *testing.T) {
+	values := []struct {
+		Locale        string
+		Input         float64
+		Expected      string
+		ExpectedError error
+	}{
+		{
+			Locale:        "bogus",
+			Input:         1421321,
+			Expected:      "",
+			ExpectedError: fmt.Errorf("locale [%s] is not valid or is not supported", "bogus"),
+		},
+		{
+			Locale:        "C",
+			Input:         679432670410421062173920121,
+			Expected:      "",
+			ExpectedError: fmt.Errorf(`strconv.ParseInt: parsing "%s": value out of range`, "67943267041042103694209843200"),
+		},
+	}
+	for _, val := range values {
+		t.Run(fmt.Sprintf("Locale:%s", val.Locale), func(t *testing.T) {
+			result, err := Format(val.Input, val.Locale)
+			if val.ExpectedError != nil {
+				assert.EqualError(t, err, val.ExpectedError.Error())
+			} else {
+				assert.NoError(t, err, "no error should be returned")
+			}
+			assert.Equal(t, val.Expected, result, "should match expected")
 		})
 	}
 }
